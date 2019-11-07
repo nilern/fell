06f8e7f4e9028829dbeda13be88ad996a00439aa
Pauli Jaakkola <pauli.jaakkola@metosin.fi>
2019-11-07 16:17:48 +0200

    Fix README and project.clj oversights.

--- README.md
+++ README.md
@@ -44,7 +44,7 @@ Don't.
 
 ## License
 
-Copyright © 2018 FIXME
+Copyright © 2019 Pauli Jaakkola
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject fell "0.1.0-SNAPSHOT"
   :description "Freer monads for Clojure"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/nilern/fell"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [funcool/cats "2.2.0"]])
+  :dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]
+                 [funcool/cats "2.0.0"]])
